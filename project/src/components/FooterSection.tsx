4ed6667e90da5eff4f503d398db056fc2ff7a3a5
theerlant <ttrinanda2@gmail.com>
2026-02-05 00:06:10 +0800

    added contact

--- project/src/components/FooterSection.tsx
+++ project/src/components/FooterSection.tsx
@@ -1,5 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
 import CountdownTimer from "./countdown/CountdownTimer";
 
 export default function FooterSection() {
@@ -15,20 +16,34 @@ export default function FooterSection() {
         <CountdownTimer />
       </section>
       <section id="footer" className="py-16 pb-8 bg-black text-white">
-        <div id="contacts" className="flex justify-center items-center gap-4">
-          <a
-            href="mailto:kadekarie43@gmail.com"
-            className="hover:scale-[110%] hover:rotate-3 transition-transform"
-          >
-            <FontAwesomeIcon icon={faEnvelope} className="text-3xl" />
-          </a>
-        </div>
+        <ul
+          id="contacts"
+          className="flex flex-wrap px-32 justify-center items-center gap-4"
+        >
+          <ContactItem href="mailto:kadekarie43@gmail.com">
+            <FontAwesomeIcon icon={faEnvelope} />
+          </ContactItem>
+          <ContactItem href="https://wa.me/6282237917395">
+            <FontAwesomeIcon icon={faWhatsapp} />
+          </ContactItem>
+        </ul>
         <p className="text-sm md:text-md text-center mt-8">
           © 2026 crafted by Theerlan & Arie, all rights reserved.
           <br />
-          Email us for inquiry.
+          Contact us for inquiry.
         </p>
       </section>
     </>
+  );
+}
+
+function ContactItem({
+  children,
+  href,
+}: React.PropsWithChildren<{ href: string }>) {
+  return (
+    <li className="hover:scale-[110%] hover:rotate-3 transition-transform text-3xl">
+      <a href={href}>{children}</a>
+    </li>
   );
 }
